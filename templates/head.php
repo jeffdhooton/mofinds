d10d5f3e00e5929393776e17ea34f14a6b8d5229
Jeff <jeffreydhooton@gmail.com>
2019-10-12 16:59:46 -0400

    first commit

---
+++ templates/head.php
@@ -0,0 +1,22 @@
+<head>
+
+	<!--- basic page needs
+   ================================================== -->
+	<meta charset="utf-8">
+
+	<!-- mobile specific metas
+   ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+
+	<!-- script
+   ================================================== -->
+	<script src="js/modernizr.js"></script>
+	<script src="js/pace.min.js"></script>
+
+	<!-- favicons
+	================================================== -->
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+  <?php wp_head(); ?>
+</head>
